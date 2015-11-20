2c33ab8e14bf46f0585104ab3893fe47f4b1893e
Kelly Truc le <tnl010@ucsd.edu>
2015-11-20 13:18:12 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	CSE_134B_HW4/src/list.html
    resolved..i think..

--- CSE_134B_HW4/js/login.js
+++ CSE_134B_HW4/js/login.js
@@ -7,21 +7,16 @@ function toggleForms() {
 
 function login(evt) {
   evt.preventDefault(); // prevent form submission
-
   Parse.initialize("V6NcQkeFHBu6SOcSYJptWFgKzgOiuc2ywEXnmL31", "Xw3yYjXIFL6tVLwN3vhmPJMYLmd4AiJI3mRUjl1l");
   var email = document.getElementById("login_email").value;
   var password = document.getElementById("login_password").value;
-
   // Find the associated username using the provided email
   var query = new Parse.Query(Parse.User);
   query.equalTo("email", email);
-
   query.find({
     success : function (results) {
       // results.length should be 1 since emails are unique
       var username = (results[0]) ? results[0].get("username") : null;
-      //alert("Username for this email is: " + username);
-
       // Login using found username
       Parse.User.logIn(username, password, {
         success : function (user) {
@@ -41,20 +36,17 @@ function login(evt) {
 
 function register(evt) {
   evt.preventDefault(); // prevent form submission
-
   Parse.initialize("V6NcQkeFHBu6SOcSYJptWFgKzgOiuc2ywEXnmL31", "Xw3yYjXIFL6tVLwN3vhmPJMYLmd4AiJI3mRUjl1l");
   var username = document.getElementById("username").value;
   var password = document.getElementById("signup_password").value;
   var email = document.getElementById("signup_email").value;
   var notification = (document.getElementById("notification").checked) ? true : false;
-
   var user = new Parse.User();
   user.set("username", username);
   user.set("password", password);
   user.set("email", email);
   user.set("notification", notification);
   user.set("notitoday", false);
-
   user.signUp(null, {
     success : function (user) {
       alert("Successfully signed up!");
